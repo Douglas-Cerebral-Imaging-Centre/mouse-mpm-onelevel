f2f1d5e5cc4a067a2d717f78853487c89a2d3f98
Jeremie Fouquet <jeremie.fouquet2@gmail.com>
2024-09-17 14:46:06 -0400

    Correct bug in if statement

--- qa/check_receiver_gains.sh
+++ qa/check_receiver_gains.sh
@@ -14,7 +14,7 @@ extract_receivergain () {
 }
 
 
-tail -n +2 "../sourcedata/scan_indices.csv" | while IFS=',' read -r sourcedata_folder subject_id session_id structural_scanidx flip1_TB1DAM_scanidx flip2_TB1DAM_scanidx mpm_pdw_scanidx mpm_mtw_scanidx mpm_t1w_scanidx; do
+tail -n +2 "bids_conversion/scan_indices.csv" | while IFS=',' read -r sourcedata_folder subject_id session_id structural_scanidx mpm_pdw_scanidx mpm_mtw_scanidx mpm_t1w_scanidx flip1_TB1DAM_scanidx flip2_TB1DAM_scanidx; do
     printf "Working on sub-%s, ses-%s\n" $subject_id $session_id
     # Extract ref power from all scans (I wanted to check if they were all the same)
     flip1_TB1DAM_receivergain=$(extract_receivergain ../sourcedata/${sourcedata_folder}/${flip1_TB1DAM_scanidx}/acqp)
@@ -24,19 +24,19 @@ tail -n +2 "../sourcedata/scan_indices.csv" | while IFS=',' read -r sourcedata_f
     mpm_t1w_receivergain=$(extract_receivergain ../sourcedata/${sourcedata_folder}/${mpm_t1w_scanidx}/acqp)
     
     # Check if all receiver gains are the same for B1 mapping
-    if [[ $flip1_TB1DAM_receivergain==$flip2_TB1DAM_receivergain ]]; then
+    if [[ "$flip1_TB1DAM_receivergain" == "$flip2_TB1DAM_receivergain" ]]; then
         printf "All receiver gains are the same value for B1 mapping: $flip1_TB1DAM_receivergain\n"
     else
         printf "\n\n\nWARNING: Reference powers are different within same session\n"
-        printf "%f\n" $flip1_TB1DAM_receivergain $flip2_TB1DAM_receivergain $mpm_pdw_receivergain $mpm_mtw_receivergain $mpm_t1w_receivergain
+        printf "%f\n" $flip1_TB1DAM_receivergain $flip2_TB1DAM_receivergain
     fi
 
     # Check if all receiver gains are the same for MPM scans
-    if [[ $mpm_pdw_receivergain==$mpm_mtw_scanidx ]] && [[ $mpm_pdw_receivergain==$mpm_t1w_scanidx ]]; then
+    if [[ "$mpm_pdw_receivergain" == "$mpm_mtw_receivergain" ]] && [[ "$mpm_pdw_receivergain" == "$mpm_t1w_receivergain" ]]; then
         printf "All receiver gains are the same value for MPM scans: $mpm_pdw_receivergain\n\n"
     else
         printf "\n\n\nWARNING: Reference powers are different within same session\n"
-        printf "%f\n" $mpm_pdw_receivergain $mpm_mtw_scanidx $mpm_t1w_scanidx
+        printf "%f\n" $mpm_pdw_receivergain $mpm_mtw_receivergain $mpm_t1w_receivergain
     fi
 
 done
